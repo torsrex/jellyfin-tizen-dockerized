c2893d27266f6c61785ba85ac547a7e784aca22e
torsrex <rex.datamaskin@gmail.com>
2023-11-13 14:47:06 +0100

    fix: exit with code if install fails

--- docker-compose-build/jellyfindeploy/deploy.sh
+++ docker-compose-build/jellyfindeploy/deploy.sh
@@ -2,5 +2,5 @@ if [ -z "$TV_IP" ]; then echo "Error: TV_IP is not set."; exit 1; fi
 echo "Starting install ..."
 sdb connect $TV_IP \
 && TV_NAME=$(sdb devices | awk '/device/{if(NR==2) print $NF}') \
-&& tizen install -n Jellyfin.wgt -t $TV_NAME
+&& tizen install -n Jellyfin.wgt -t $TV_NAME || exit 1
 echo "Done installing"
